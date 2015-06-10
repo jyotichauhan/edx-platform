ab0b210a972bbba6c2deee859078fa037b662d63
Usman Khalid <2200617@gmail.com>
2015-06-11 00:55:38 +0500

    Disable task.

--- openedx/core/djangoapps/bookmarks/signals.py
+++ openedx/core/djangoapps/bookmarks/signals.py
@@ -8,7 +8,7 @@ from django.dispatch.dispatcher import receiver
 from xmodule.modulestore.django import SignalHandler
 
 
-@receiver(SignalHandler.course_published)
+#@receiver(SignalHandler.course_published)
 def trigger_update_xblocks_cache_task(sender, course_key, **kwargs):  # pylint: disable=invalid-name,unused-argument
     """
     Trigger update_xblocks_cache() when course_published signal is fired.
